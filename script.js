40b2fbd645a4fad6c7a533f5c17552a6d0555c71
PEDRO RAFAEL LIMA BEZERRA DE MEDEIROS <pedromedeiros0978@digitalcollege.internal>
2025-06-03 20:18:32 -0300

    Executando JS no HTML

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 function converterCtoF(tempC) {
-    const tempF = (tempC * 9/5) + 32
+    const tempF = (tempC * 1.8) + 32
     console.log(`Temperatura = ${tempF} F`);
+
+    return tempF;
 }
 
-function convert
+function convertCtoK(tempC) {
+    
+}
+
+function converter() {
+    console.log('Ei Clicou!');
+    let tempC = document.getElementById('TempC').value
+    console.log(`Temp °C: ${tempC}`);
+    /* Chamando a função que converte de C para F*/
+    const resultado = converterCtoF(tempC);
+
+    document.getElementById('resultado').innerHTML = resultado;
+}
 
 converterCtoF(18)
 converterCtoF(30)
